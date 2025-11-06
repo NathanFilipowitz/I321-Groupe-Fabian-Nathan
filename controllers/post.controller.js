8f1ba2c76120b981dfbe1564806ff2f88bdb379e
Fabian <rostellofabian@gmail.com>
2025-11-06 11:07:16 +0100

    chore: added folder structure

---
+++ controllers/post.controller.js
@@ -0,0 +1,19 @@
+const postcontroller = {
+    updatePost: async (req, res) => {
+        const errorMsgs = joiValidator(
+            req.body,
+            postSchemas.updatePostSchema
+        );
+        if (errorMsgs) {
+            return responseSender.sendValidationError(res, errorMsgs);
+        }
+        const post = await postService.updatePost(
+            req.user.userId,
+            req.body
+        );
+        return responseSender.sendSuccessResponse(res, post);
+    },
+
+}
+
+export { postcontroller }
