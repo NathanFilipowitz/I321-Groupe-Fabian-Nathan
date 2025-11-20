eea1eec06e893cb42e98490073c49cc4d9c1f51e
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-20 11:59:57 +0100

    adapted bettere routing logic (replaced /api/pizza with /pizzas and /pizzas/id

--- controllers/pizza.controller.js
+++ controllers/pizza.controller.js
@@ -8,14 +8,14 @@ const pizzaController = {
         const pizza = await pizzaService.getPizzaById(pizzaId);
 
         const message = `La pizza avec l'id ${pizzaId} a bien été trouvé !`;
-        return responseSender.sendSuccessResponse(res, pizza, message);
+        return responseSender.sendSuccessResponse(pizza, message);
 
     },
     getAllPizzas: async (req,res) => {
         const pizzas = await pizzaService.getAllPizzas();
 
         const message = `Toutes les pizzas ont bien été trouvées`;
-        return responseSender.sendSuccessResponse(res, pizzas, message);
+        return responseSender.sendSuccessResponse(pizzas, message);
     }
 };
 
