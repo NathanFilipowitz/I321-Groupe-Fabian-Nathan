b502fe27c7a1bc0f19cec5f65dd67918b4bf7e16
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-12-11 10:22:46 +0100

    feat: add route to change pizza price by ID

--- routes/pizza.route.js
+++ routes/pizza.route.js
@@ -4,7 +4,6 @@ import asyncHandler from "../helpers/asyncRouteHandler.helper.js";
 
 const router = Router();
 
-// Utilisez le wrapper asyncHandler
 router.get('/pizzas', asyncHandler(pizzaController.getAllPizzas));
 router.get('/pizzas/:id/ingredients', asyncHandler(pizzaController.getIngredientsByPizzaId));
 router.get('/pizzas/:id', asyncHandler(pizzaController.getPizzaById));
@@ -12,10 +11,9 @@ router.get('/ingredients', asyncHandler(pizzaController.getAllIngredients));
 router.get('/ingredients/:id', asyncHandler(pizzaController.getIngredientById));
 router.get('/daily', asyncHandler(pizzaController.getPizzaOfTheDay));
 
-router.post("/ingredients", asyncHandler(pizzaController.createNewIngredient));
-router.post("/pizzas", asyncHandler(pizzaController.createNewPizza));
-// router.put("/",asyncHandler(pizzaController.updatePost));
-// router.delete("/:postId", asyncHandler(pizzaController.deletePost));
-// router.get("/", asyncHandler(pizzaController.getAllUserPosts));
+router.post('/ingredients', asyncHandler(pizzaController.createNewIngredient));
+router.post('/pizzas', asyncHandler(pizzaController.createNewPizza));
+
+router.patch('/pizzas/:id/price', asyncHandler(pizzaController.changePizzaPrice));
 
 export default router;
